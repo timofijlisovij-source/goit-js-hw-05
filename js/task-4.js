32d4d542ac1eeb0d1cc43859967f17aeb2885d57
timofijlisovij-source <timofijlisovij@gmail.com>
2026-04-21 14:15:27 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,5 @@
 const getTotalBalanceByGender = (users, gender) =>
-	users
-.filter(({gender: sex}) => sex === gender)
+users.filter(({gender: sex}) => sex === gender)
 .reduce((mid, {balance}) => mid + balance, 0)
 
 
